aca7ee9d9a24587a35a2925558fc921956d76b89
Mxlu42 <166992193+Mxlu42@users.noreply.github.com>
2025-06-17 13:16:45 +0200

    faecherausklammern und bestanden eingefügt

---
+++ dist/scripts/__pycache__/faecherAusklammern.py
@@ -0,0 +1,34 @@
+from dbhelp import *
+from bestanden import *
+
+class FaecherRausstreichen(object):
+    def __init__(self, a):                      #a ist ein Array, das die fächer die er rausstreichen  möchte beinhaltet. im Array stehen: Alles von dem Fach (um die daten zu haben, falls der Try falsch ist)
+        self.Tryfaecher = a 
+        self.spanischbelegt = DBHelp.coutFachBelegt('SpanischN')
+        if self.Streichen() == False or self.Ueberpuefen() == False:
+            for i in self.Tryfaecher:
+                DBHelp.setBelegt(i(1), i(2), True)
+            return False
+        return True
+
+    def Streichen(self):
+        pruefungfaecher = DBHelp.getpruefungsfacher()   #return ist ein eindimensionales Array das die fachnamen der pruefungsfaecher beinhaltet
+        for i in self.Tryfaecher:
+            for u in pruefungfaecher:
+                if i(2) == u:
+                    return False
+            if i(2) == ('Deutsch' or 'Mathe' or 'GGK' or 'Physik' or 'Chemie') or i(3) == 'Profil' or DBHelp.coutFachBelegt('Informatik') < 2:     #i(3) soll Fachtype sein #in hj 3,4,5,6
+                return False
+            DBHelp.setBelegt(i(1), i(2), False)       #i(1) soll halpjahr und i(2) das fach sein
+        return True
+    
+    def Ueberpuefen(self):
+        if DBHelp.CountFaecherBelegt()  < 36:             #in hj 3,4,5,6
+            return False
+        if self.spanischbelegt == 0 and DBHelp.coutFachBelegt('Englisch') != 4:
+            return False
+        if (self.spanischbelegt > 0 and DBHelp.coutFachBelegt('Englisch') < 4 and DBHelp.coutFachBelegt('Spanisch') < 2) or self.spanischbelegt > 0 and DBHelp.coutFachBelegt('Spanisch') < 4:
+            return False
+        if Bestanden.AbiJahreBestanden() == False or Bestanden.Dreizehn1Bestanden() == False or Bestanden.Dreizehn2Bestanden() == False or Bestanden.Zwoelfte1Bestanden() == False or Bestanden.Zwoelfte2Bestanden() == False:
+            return False
+        return True
